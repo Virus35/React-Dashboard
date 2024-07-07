f599287a6f50dd4bbe75191bcf883b6ae18c4d15
Vishal Rikhi <vishal.rikhi@paxcom.net>
2024-07-07 15:12:07 +0530

    build fixed

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { FaTags, FaChartBar, FaFileInvoice, FaUtensils } from "react-icons/fa";
 import { IoStorefrontSharp } from "react-icons/io5";
